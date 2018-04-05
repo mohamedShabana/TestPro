ff8a56095b556a5b61512ac9c15873866f6cf70e
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2018-04-05 14:42:49 +0200

    add Code to user and Role in startUp

--- WebApp/WebApp/Startup.cs
+++ WebApp/WebApp/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using WebApp.Models;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(WebApp.Startup))]
@@ -6,9 +9,40 @@ namespace WebApp
 {
     public partial class Startup
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateUs();
+        }
+        public void CreateUs()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("Adminstrator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Adminstrator";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "adam";
+                user.Email = "adam@pplus.com";
+                string pass = "Java@123";
+                var ch = UserManager.Create(user, pass);
+                if (ch.Succeeded)
+                {
+                    var result = UserManager.AddToRole(user.Id, "Adminstrator");
+                }
+            }
+            if (!roleManager.RoleExists("student"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "student";
+                roleManager.Create(role);
+            }
         }
     }
 }
